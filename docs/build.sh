153f3ecedeba15c584a786ac6986e12f1cf473b0
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2024-07-12 16:39:43 +0100

    add landing page redirect

--- docs/build.sh
+++ docs/build.sh
@@ -4,4 +4,6 @@ mkdir build
 
 touch build/.nojekyll  # Disable jekyll to keep files starting with underscores
 
+cp ./_static/_redirect.html ./build/index.html
+
 sphinx-build -b html ./api-docs ./build/api-docs
